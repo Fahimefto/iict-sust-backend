3fe66b84466eaf22d7222e21320105b01f03d567
Fahimefto <ifto.141.fi@gmail.com>
2022-09-27 15:10:25 +0600

    Database setup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +11,18 @@ app.get("/", (req, res) => {
   res.json({ message: "Server Running " });
 });
 
-app.listen(process.env.PORT, () =>
-  console.log("listening at " + process.env.PORT)
-);
+app.listen(process.env.PORT, async () => {
+  console.log("listening at " + process.env.PORT);
+  try {
+    const con = await mongoose.connect(process.env.MONGO_URI_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(
+      `database connected  : ${con.connection.host} \ndatabase name : ${con.connection.name}`
+    );
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+});
