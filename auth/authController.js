cd61c22209066f850f2600d1a2e472e7cdf8307e
Abidstic101 <abid71@student.sust.edu>
2023-03-12 15:03:41 +0600

    created refresh tokens and stored in db

--- auth/authController.js
+++ auth/authController.js
@@ -1,7 +1,12 @@
-const AuthUser = require("../models/user");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const AuthUser = require('../models/user');
+const RefreshToken = require('../models/refreshToken');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+    signAccessToken,
+    signRefreshToken,
+} = require('../middleware/token_generator');
 
 //login user
 const authLogin = async (req, res) => {
@@ -9,29 +14,28 @@ const authLogin = async (req, res) => {
     try {
         const user = await AuthUser.findOne({ email });
         if (!user) {
-            return res.status(404).json({ message: "User not found" });
+            return res.status(404).json({ message: 'User not found' });
         } else {
             const isValid = await bcrypt.compare(password, user.password);
             if (isValid) {
-                const token = jwt.sign(
-                    {
-                        user_info: {
-                            id: user._id,
-                            role : user.role
-                        },
-                    },
-                    process.env.JWT_MAGIC,
-                    {
-                        expiresIn: "12h",
-                    }
+                const accessToken = await signAccessToken(user._id, user.role);
+                const refreshToken = await signRefreshToken(
+                    user._id,
+                    user.role
                 );
+
+                await RefreshToken.create({
+                    token: refreshToken,
+                });
+
                 res.status(200).json({
-                    message: "Login Success",
+                    message: 'Login Success',
                     auth: true,
-                    token,
+                    accessToken,
+                    refreshToken,
                 });
             } else {
-                res.status(400).json({ message: "Invalid credentials" });
+                res.status(400).json({ message: 'Invalid credentials' });
             }
         }
     } catch (error) {
@@ -41,15 +45,15 @@ const authLogin = async (req, res) => {
 
 //register user
 const authRegister = async (req, res) => {
-    const { username, fullname, email, password, phoneNum,role} = req.body;
+    const { username, fullname, email, password, phoneNum, role } = req.body;
     try {
         if (!username || !fullname || !email || !password || !phoneNum)
             return res
                 .status(400)
-                .json({ message: "Please fill in all fields" });
+                .json({ message: 'Please fill in all fields' });
         const user = await AuthUser.findOne({ email });
         if (user)
-            return res.status(400).json({ message: "user already exists" });
+            return res.status(400).json({ message: 'user already exists' });
         const hashedPassword = await bcrypt.hash(password, 9);
         const newUser = await AuthUser.create({
             username,
@@ -59,6 +63,7 @@ const authRegister = async (req, res) => {
             phoneNum,
             role,
         });
+
         res.status(200).json(newUser);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -68,8 +73,12 @@ const authRegister = async (req, res) => {
 //show all users
 const getAllUsers = async (req, res) => {
     try {
-        const users = await AuthUser.find();
-        res.status(200).json(users);
+        if (req.role === 'staff') {
+            const users = await AuthUser.find();
+            res.status(200).json(users);
+        } else {
+            res.status(403).json({ message: 'You are not authorized' });
+        }
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -78,9 +87,9 @@ const getAllUsers = async (req, res) => {
 //update user
 const updateUserByID = async (req, res) => {
     const { id } = req.params;
-    const { fullname, email, password, phoneNum,role} = req.body;
+    const { fullname, email, password, phoneNum, role } = req.body;
     if (!mongoose.Types.ObjectId.isValid(id))
-        return res.status(404).json({ message: "No user with that id" });
+        return res.status(404).json({ message: 'No user with that id' });
     const updatedUser = {
         fullname,
         email,
@@ -89,16 +98,52 @@ const updateUserByID = async (req, res) => {
         role,
     };
     await AuthUser.findByIdAndUpdate({ _id: id }, updatedUser, { new: true });
-    res.json({ message: "User updated successfully", updatedUser });
+    res.json({ message: 'User updated successfully', updatedUser });
 };
 
 //delete user by id
 const deleteUserByID = async (req, res) => {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id))
-        return res.status(404).json({ message: "No user with that id" });
+        return res.status(404).json({ message: 'No user with that id' });
     await AuthUser.findByIdAndRemove({ _id: id });
-    res.json({ message: "User deleted successfully" });
+    res.json({ message: 'User deleted successfully' });
+};
+
+const refreshToken = async (req, res) => {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+        return res.status(401).json({ message: 'No token provided' });
+    } else {
+        const validtoken = await RefreshToken.findOne({ token: refreshToken });
+        if (!validtoken) {
+            return res.status(403).json({ message: 'Invalid token' });
+        } else {
+            jwt.verify(
+                refreshToken,
+                process.env.JWT_MAGIC_REFRESH,
+                async (err, decode) => {
+                    if (err)
+                        return res.status(403).json({
+                            message: 'Invalid token',
+                        });
+                    const user_id = decode.user_info.id;
+                    const user_role = decode.user_info.role;
+
+                    const accToken = await signAccessToken(user_id, user_role);
+                    const refToken = await signRefreshToken(user_id, user_role);
+                    await RefreshToken.replaceOne(validtoken, {
+                        token: refToken,
+                    });
+                    res.status(200).json({
+                        message: 'Token refreshed',
+                        accessToken: accToken,
+                        refreshToken: refToken,
+                    });
+                }
+            );
+        }
+    }
 };
 
 module.exports = {
@@ -107,4 +152,5 @@ module.exports = {
     getAllUsers,
     updateUserByID,
     deleteUserByID,
+    refreshToken,
 };
--- models/user.js
+++ models/user.js
@@ -1,19 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const authUserSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true },
-    fullname: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    phoneNum: { type: Number, required: true },
-    role: {
-      type: String,
-      enum: ["admin", "teacher", "staff"],
-      required: [true, "Please provide a role"],
-      default: "staff",
-    }
-  },
-  { timestamps: true }
+    {
+        username: { type: String, required: true },
+        fullname: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        phoneNum: { type: String, required: true },
+        
+       
+        role: {
+            type: String,
+            enum: ['admin', 'teacher', 'staff'],
+            required: [true, 'Please provide a role'],
+            default: 'staff',
+        },
+    },
+    { timestamps: true }
 );
 
-module.exports = mongoose.model("AuthUser", authUserSchema);
+module.exports = mongoose.model('AuthUser', authUserSchema);
